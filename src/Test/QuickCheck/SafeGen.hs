66cf2cca51b09e21485229a85f9f40b942e001ea
Jonas Carpay <jonascarpay@gmail.com>
2023-04-10 02:07:02 +0900

    runSafeGen

--- src/Test/QuickCheck/SafeGen.hs
+++ src/Test/QuickCheck/SafeGen.hs
@@ -1,44 +1,106 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
-module Test.QuickCheck.SafeGen where
+module Test.QuickCheck.SafeGen
+  ( choice,
+    runSafeGen,
+    gen,
+    SafeGen,
+  )
+where
 
+import Data.Foldable (toList)
 import Data.List.NonEmpty (NonEmpty (..))
 import Test.QuickCheck
+
+-- TODO memoize cost
 
 data Nat = Zero | Succ Nat
   deriving (Show)
 
-infinity :: Nat
-infinity = Succ infinity
-
+-- TODO rename
 safeMin :: Traversable t => t Nat -> Nat
-safeMin xs = case traverse peel xs of
+safeMin xs = case traverse unsucc xs of
   Nothing -> Zero
   Just xs' -> Succ $ safeMin xs'
+  where
+    unsucc :: Nat -> Maybe Nat
+    unsucc Zero = Nothing
+    unsucc (Succ l) = Just l
 
-peel :: Nat -> Maybe Nat
-peel Zero = Nothing
-peel (Succ l) = Just l
+-- TODO rename
+safeMinCost :: Traversable t => t (SafeGen a) -> NonEmpty (SafeGen a)
+safeMinCost = go . fmap (\x -> (x, cost x))
+  where
+    unpeel (x, Zero) = VLeft (pure x)
+    unpeel (x, Succ n) = VRight (x, n)
+    go xs = case traverse unpeel xs of
+      VLeft a -> a
+      VRight xs' -> go xs'
 
 data SafeGen a
   = Gen (Gen a)
   | forall i.
     Product
-      { _leftArity :: Double,
+      { _leftArity :: !Double,
         _leftBranch :: SafeGen (i -> a),
-        _rightArity :: Double,
+        _rightArity :: !Double,
         _rightBranch :: SafeGen i
       }
   | Choice (NonEmpty (SafeGen a))
 
+runSafeGen :: SafeGen a -> Gen a
+runSafeGen sg = sized (\size -> go (fromIntegral size) sg)
+  where
+    go :: Double -> SafeGen a -> Gen a
+    go budget (Gen g) = resize (floor budget) g
+    go budget (Product al l ar r) = let a = al + ar in go (budget * al / a) l <*> go (budget * ar / a) r
+    go budget (Choice as) = case filter (flip leqDouble budget . cost) (toList as) of
+      [] -> oneof (go budget <$> toList (safeMinCost as))
+      as' -> oneof (go budget <$> as')
+
+leqDouble :: Nat -> Double -> Bool
+leqDouble Zero !x = x > 0
+leqDouble (Succ n) !x = x > 1 && leqDouble n (x - 1)
+
+deriving instance Functor SafeGen
+
+instance Applicative SafeGen where
+  pure = Gen . pure
+  a <*> b = Product (arity a) a (arity b) b
+    where
+      arity :: SafeGen q -> Double
+      arity (Product l _ r _) = l + r
+      arity _ = 1
+
+gen :: Gen a -> SafeGen a
+gen = Gen
+
 choice :: [SafeGen a] -> SafeGen a
-choice [] = error "asd"
+choice [] = error "SafeGen.choice: empty list"
 choice (a : as) =
   let ne = a :| as
    in Choice ne
 
 cost :: SafeGen a -> Nat
-cost (Gen _) = Zero
-cost (Choice as) = Succ $ safeMin (cost <$> as)
+cost (Gen _) = Succ Zero
+cost (Choice as) = safeMin (cost <$> as)
+cost (Product _ l _ r) = cost l `addNat` cost r
+  where
+    addNat :: Nat -> Nat -> Nat
+    addNat Zero b = b
+    addNat (Succ a) b = Succ (addNat a b)
+
+data Validation e a = VLeft (NonEmpty e) | VRight a
+  deriving (Functor)
+
+instance Applicative (Validation e) where
+  pure = VRight
+  VRight f <*> VRight a = VRight (f a)
+  VLeft e1 <*> VLeft e2 = VLeft (e1 <> e2)
+  VLeft e1 <*> _ = VLeft e1
+  _ <*> VLeft e2 = VLeft e2
--- test/Spec.hs
+++ test/Spec.hs
@@ -10,62 +10,62 @@ main =
       it "works" $
         2 + 2 `shouldBe` (4 :: Int)
 
-loops = choice [choice [Gen undefined], loops]
+-- loops = choice [choice [Gen undefined], loops]
 
-terminates = choice [terminates, Gen undefined]
+-- terminates = choice [terminates, Gen undefined]
 
-data Trie a
-  = Leaf a
-  | Branch a (Trie a) (Trie a) (Trie a)
-  deriving (Show, Foldable)
+-- data Trie a
+--   = Leaf a
+--   | Branch a (Trie a) (Trie a) (Trie a)
+--   deriving (Show, Foldable)
 
-instance Arbitrary a => Arbitrary (Trie a) where
-  arbitrary = runSafeGenGrow go
-    where
-      go =
-        Safe.oneof
-          [ con1 Leaf (gen arbitrary),
-            con4 Branch (gen arbitrary) go go go
-          ]
+-- instance Arbitrary a => Arbitrary (Trie a) where
+--   arbitrary = runSafeGenGrow go
+--     where
+--       go =
+--         Safe.oneof
+--           [ con1 Leaf (gen arbitrary),
+--             con4 Branch (gen arbitrary) go go go
+--           ]
 
-data Term a
-  = Var a
-  | Lam (Term (Maybe a))
-  | App (Term a) (Term a)
-  deriving (Show)
+-- data Term a
+--   = Var a
+--   | Lam (Term (Maybe a))
+--   | App (Term a) (Term a)
+--   deriving (Show)
 
-ball :: Gen (Trie ())
-ball = runSafeGenGrow go
-  where
-    go =
-      Safe.frequency
-        [ (1, con1 Leaf (gen arbitrary)),
-          (1, con4 Branch (gen arbitrary) (Logic.once go) go go)
-        ]
+-- ball :: Gen (Trie ())
+-- ball = runSafeGenGrow go
+--   where
+--     go =
+--       Safe.frequency
+--         [ (1, con1 Leaf (gen arbitrary)),
+--           (1, con4 Branch (gen arbitrary) (Logic.once go) go go)
+--         ]
 
-nobal :: Gen (Trie ())
-nobal = runSafeGenGrow go
-  where
-    go =
-      Safe.frequency
-        [ (1, Leaf <$> gen arbitrary),
-          (1, Branch <$> gen arbitrary <*> go <*> go <*> go)
-        ]
+-- nobal :: Gen (Trie ())
+-- nobal = runSafeGenGrow go
+--   where
+--     go =
+--       Safe.frequency
+--         [ (1, Leaf <$> gen arbitrary),
+--           (1, Branch <$> gen arbitrary <*> go <*> go <*> go)
+--         ]
 
-tbal :: Trie a -> Maybe (Int, Int, Int)
-tbal (Leaf _) = Nothing
-tbal (Branch _ a b c) = Just (length a, length b, length c)
+-- tbal :: Trie a -> Maybe (Int, Int, Int)
+-- tbal (Leaf _) = Nothing
+-- tbal (Branch _ a b c) = Just (length a, length b, length c)
 
-instance Arbitrary a => Arbitrary (Term a) where
-  arbitrary = runSafeGenGrow (go arb)
-    where
-      go :: SafeGen a -> SafeGen (Term a)
-      go x =
-        Safe.oneof
-          [ Var <$> x,
-            con2 App (go x) (go x),
-            con1 Lam (go (Safe.oneof [pure Nothing, Just <$> x]))
-          ]
+-- instance Arbitrary a => Arbitrary (Term a) where
+--   arbitrary = runSafeGenGrow (go arb)
+--     where
+--       go :: SafeGen a -> SafeGen (Term a)
+--       go x =
+--         Safe.oneof
+--           [ Var <$> x,
+--             con2 App (go x) (go x),
+--             con1 Lam (go (Safe.oneof [pure Nothing, Just <$> x]))
+--           ]
 
-sampleBig :: Show a => Gen a -> IO ()
-sampleBig m = forM_ [1, 5 .. 100] $ \s -> generate (resize s m) >>= print
+-- sampleBig :: Show a => Gen a -> IO ()
+-- sampleBig m = forM_ [1, 5 .. 100] $ \s -> generate (resize s m) >>= print
